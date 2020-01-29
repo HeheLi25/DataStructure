3239736f6994611388ba5642beb9b7b4d37232e3
liyirong_Surface <li.yirong.z25@gmail.com>
2020-01-29 16:21:40 +0000

    week3

---
+++ src/CourseExercise/BinaryConvert.java
@@ -0,0 +1,30 @@
+package CourseExercise;
+
+public class BinaryConvert {
+	public static String notRec(int n) {
+		String result = "";
+		int remain = 0;
+		while(n>0) {
+			remain = n%2;
+			result = remain + result;
+			n = n/2;
+		}
+		return result;
+	}
+	
+	public static String recBinary(int n) {
+		if(n == 0) {
+			return "";
+		}else {
+			int remain = n%2;
+			return recBinary(n/2)+remain;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(notRec(243));
+		System.out.println(recBinary(243));
+		
+	}
+
+}
